1637d622f6bbe12e05ad97a294721dd667d0838b
Cathy Pham-Le <cathyphamle@berkeley.edu>
2017-04-04 17:44:17 -0700

    starter file

---
+++ iOSDecalCustomApp/iOSDecalCustomApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSDecalCustomApp
+//
+//  Created by Cathy Pham Le on 4/4/17.
+//  Copyright © 2017 Cathy Pham Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
