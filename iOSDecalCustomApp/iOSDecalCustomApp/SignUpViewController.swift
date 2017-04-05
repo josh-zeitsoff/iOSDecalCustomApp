1ea65d59e201480fa9964e270a362af70aa4527f
Cathy Pham-Le <cathyphamle@berkeley.edu>
2017-04-04 18:37:28 -0700

    sign up VC

---
+++ iOSDecalCustomApp/iOSDecalCustomApp/SignUpViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SignUpViewController.swift
+//  iOSDecalCustomApp
+//
+//  Created by Cathy Pham Le on 4/4/17.
+//  Copyright © 2017 Cathy Pham Le. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var EmailInput: UITextField!
+    
+    @IBOutlet weak var UserNameSignUpInput: UITextField!
+    
+    @IBOutlet weak var PasswordSignUpInput: UITextField!
+    
+    @IBAction func RegisterButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
