7d64841ff2095bb8ec6852a0d4adb19dbfd9f5ac
Cathy Pham-Le <cathyphamle@berkeley.edu>
2017-04-20 19:57:45 -0700

    doing tableview stuff

--- iOSDecalCustomApp/iOSDecalCustomApp/EventDashboardViewController.swift
+++ iOSDecalCustomApp/iOSDecalCustomApp/EventDashboardViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 import Firebase
 
-class EventDashboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class EventDashboardViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, FIRInviteDelegate {
+    
+    
+    var ref: FIRDatabaseReference!
+    var events: [FIRDataSnapshot]! = []
+    fileprivate var _refHandle: FirDatabaseHandle!
 
     @IBOutlet weak var EventDashboardTableView: UITableView!
     @IBAction func CreateEventButton(_ sender: Any) {
@@ -20,9 +25,55 @@ class EventDashboardViewController: UIViewController, UITableViewDelegate, UITab
         
     }
     
+    @IBAction func signOut(_ sender: UIButton) {
+        let firebaseAuth = FIRAuth.auth()
+        do {
+            try firebaseAuth?.signOut()
+            dismiss(animated: true, completion: nil)
+        } catch let signOutError as NSError {
+            print ("Error signing out: \(signOutError.localizedDescription)")
+        }
+    }
+    
+    deinit {
+        if let refHandle = _refHandle {
+            self.ref.child("events").removeObserver(withHandle: _refHandle)
+        }
+    }
+    
+    
+    func configureDatabase() {
+        ref = FIRDatabase.database().reference()
+        // Listen for new messages in the Firebase database
+        _refHandle = self.ref.child("messages").observe(.childAdded, with: { [weak self] (snapshot) -> Void in
+            guard let strongSelf = self else { return }
+            strongSelf.messages.append(snapshot)
+            strongSelf.clientTable.insertRows(at: [IndexPath(row: strongSelf.messages.count-1, section: 0)], with: .automatic)
+        })
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Dequeue cell
+        let cell = EventDashboardTableView.dequeueReusableCell(withIdentifier: "EventDashboardTableViewCell", for: indexPath)
+        // Unpack message from Firebase DataSnapshot
+        let eventSnapshot = EventDashboardTableView.messages[indexPath.row]
+        guard let event = eventSnapshot.value as? [String: String] else { return cell }
+        let name = events[Constants.MessageFields.name] ?? ""
+        let text = events[Constants.MessageFields.text] ?? ""
+        cell.textLabel?.text = name + ": " + text
+        cell.imageView?.image = UIImage(named: "ic_account_circle")
+        if let photoURL = message[Constants.MessageFields.photoURL], let URL = URL(string: photoURL),
+            let data = try? Data(contentsOf: URL) {
+            cell.imageView?.image = UIImage(data: data)
+        }
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        EventDashboardTableView.delegate = self
+        EventDashboardTableView.source = self
         // Do any additional setup after loading the view.
     }
 
