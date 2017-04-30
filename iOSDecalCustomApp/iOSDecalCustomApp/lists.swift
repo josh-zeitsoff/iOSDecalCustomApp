956d65184c9e9da2c76d415c978de1c16baab0d9
Ali Mousa <amousa11@gmail.com>
2017-04-29 20:43:27 -0700

    merge

--- iOSDecalCustomApp/iOSDecalCustomApp/lists.swift
+++ iOSDecalCustomApp/iOSDecalCustomApp/lists.swift
@@ -5,7 +5,6 @@
 //  Created by Angela Wong on 4/5/17.
 //  Copyright © 2017 Cathy Pham Le. All rights reserved.
 //
-
 import Foundation
 import UIKit
 import FirebaseStorage
@@ -14,13 +13,12 @@ import FirebaseDatabase
 //will change later, prob not string
 var events: [String: [String]] = ["Current Events": [], "Invited To": []]
 //
-
 let eventTypes = ["Current Events", "Invited To"]
 
 func getEvent(indexPath: IndexPath) -> String? {
     let eventType = eventTypes[indexPath.section]
-    if let eventsArray = events[eventType] {
-        return eventsArray[indexPath.row]
+    if let event = events[eventType] {
+        return event[indexPath.row]
     }
     return nil
     
@@ -37,56 +35,39 @@ func getPerson(indexPath: IndexPath) -> String? {
     return nil
 }
 
-func addEvent(date: String, host: String, location: String, username: String) {
+func addInvite(eventID: String, userID: String, count: Int) {
     let dbRef = FIRDatabase.database().reference()
-
-    //let path = "\(firStorageImagesPath)/\(UUID().uuidString)"
-    
-    
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = dateFormat
-    let date = dateFormatter.string(from: Date())
-    
     let dict: [String:AnyObject] = [
-        "date": date as AnyObject,
-        "host" : host as AnyObject,
-        //"invitedPeople" : invitedPeople as [String] as AnyObject,
-        "location" : location as AnyObject,
-        "username" : username as AnyObject
+        "userid": userID as AnyObject,
+        "eventid": eventID as AnyObject,
+        "count": count as AnyObject
     ]
-    
-    dbRef.child(firEventsNode).childByAutoId().setValue(dict)
-    // YOUR CODE HERE
+    dbRef.child(firInvitesNode).childByAutoId().setValue(dict)
 }
 
-func getEvents(user: CurrentUser, completion: @escaping ([Event]?) -> Void) {
+func getInvites(user: CurrentUser, completion: @escaping ([Invites]?) -> Void) {
     let dbRef = FIRDatabase.database().reference()
-    var eventArray: [Event] = []
-    dbRef.child(firEventsNode).observeSingleEvent(of: .value, with: {
+    var inviteArray: [Invites] = []
+    dbRef.child(firInvitesNode).observeSingleEvent(of: .value, with: {
         (snapshot) in
         if snapshot.exists() {
-            if let eventsDict = snapshot.value as? [String : AnyObject] {
-                user.getInvitedEvents(completion: { (usersInvitedEvents) in
-                    for key in eventsDict.keys {
-                        //double check only adding invited events
-                        if usersInvitedEvents.contains(key) {
-                            let username = eventsDict[key]?["username"] as! String
-                            let name = eventsDict[key]?["name"] as! String
-                            let location = eventsDict[key]?["location"] as! String
-                            let host = eventsDict[key]?["host"] as! String
-                            let invitedPeople = eventsDict[key]?["invitedPeople"] as! [String]
-                            let date = eventsDict[key]?["date"] as! String
-                            let event = Event(dateString: date, host: host, invitedPeople: invitedPeople, location: location, name: name, id: key)
-                            eventArray.append(event)
-                        }
-                        
-                    }
-                    completion(eventArray)
-                })
+            if let invitesDict = snapshot.value as? [String : AnyObject] {
+                for key in invitesDict.keys {
+                    let userid = invitesDict[key]?["userid"] as! String
+                    let eventid = invitesDict[key]?["eventid"] as! String
+                    let count = invitesDict[key]?["count"] as! String
+                    let invite = Invites.init(eventID: eventid, userID: userid, count: count)
+                    inviteArray.append(invite)
+                }
+                completion(inviteArray)
             }
         }
+            //make query
         else {
             completion(nil)
         }
+        //else {
+        //completion(nil)
+        //}
     })
 }
